14a4a6b93b0ff6db7ddf7ca0e93147ba463541a4
Kazi Arafat <arafatkazi2448@gmail.com>
2022-01-09 02:02:05 +0530

    Initial Structure Created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { AddMeasureComponent } from './components/add-measure/add-measure.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    AddMeasureComponent,
+    LoginComponent,
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/add-measure/add-measure.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-measure',
+  templateUrl: './add-measure.component.html',
+  styleUrls: ['./add-measure.component.css']
+})
+export class AddMeasureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
